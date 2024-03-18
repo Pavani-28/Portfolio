c97ac0bcdb999071e2bf286bd3c55cf0da8f5e2a
Pavani-28 <pavanimuthyam28@gmail.com>
2024-03-18 17:52:22 +0530

    changes

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -8,13 +8,14 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi Everyone, I am <span className="purple">Soumyajit Behera </span>
-            from <span className="purple"> Bhubaneswar, India.</span>
+            Hi Everyone, I am <span className="purple">Pavani Muthyam </span>
+            from <span className="purple"> Andhra Pradesh, India.</span>
             <br />
-            I am currently employed as a software developer at Juspay.
+            I am currently a final year student!
+            <br/>
+            College: Sree Vidyanikethan Engineering College
             <br />
-            I have completed Integrated MSc (IMSc) in Maths and Computing at BIT
-            Mesra.
+            Branch:Computer Science
             <br />
             <br />
             Apart from coding, some other activities that I love to do!
@@ -24,7 +25,7 @@ function AboutCard() {
               <ImPointRight /> Playing Games
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs
+              <ImPointRight /> Reading blogs
             </li>
             <li className="about-activity">
               <ImPointRight /> Travelling
@@ -34,7 +35,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Pavani</footer>
         </blockquote>
       </Card.Body>
     </Card>
